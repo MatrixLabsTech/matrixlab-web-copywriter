b0bf7b2a3706c76bcedcbc98c09104c20c2f1904
xia <xiadd0102@gmail.com>
2022-01-25 14:44:30 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,13 +1,68 @@
 export default {
-  "home": {
-    "title": "Home - Matrix Labs",
+  home: {
+    title: "Home - Matrix Labs",
     "leading-title": "Bringing the mataverse to life",
     "leading-text": "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain.",
-    "projects": [
+    projects: [
       {
-        "title": "Matrix World",
-        "detail": "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain."
-      }
+        title: "Matrix World",
+        cover: "/images/matrix.png",
+        statistics: [
+          {
+            leading: "1",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "10M+",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "$700M+",
+            sub: "Daap in the world"
+          },
+        ],
+        detail: "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain."
+      },
+      
+      {
+        title: "Phanta Bear",
+        cover: "/images/bear.png",
+        statistics: [
+          {
+            leading: "1",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "10M+",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "$700M+",
+            sub: "Daap in the world"
+          },
+        ],
+        detail: "The PhantaBear project was jointly launched by Jay Chou's PHANTACi and Ezek. PhantaBear is a collection of 10,000 algorithmically generated digital collectibles that double as membership cards for the Ezek Club. Each PhantaBear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner."
+      },
+      
+      {
+        title: "Rivermen",
+        cover: "/images/rivermen.png",
+        statistics: [
+          {
+            leading: "1",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "10M+",
+            sub: "Daap in the world"
+          },
+          {
+            leading: "$700M+",
+            sub: "Daap in the world"
+          },
+        ],
+        detail: "Rivermen is an Oriental Cyber Theme Park based on blockchain technology, launched by the Cthuwork&NA team using elements from the voxel version of the “Along the River During Qingming Festival”. Each unique Rivermen NFT is a randomized 3D character model."
+      },
     ]
   }
 }
