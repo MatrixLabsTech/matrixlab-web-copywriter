47abe1a7b986ef46ae5e72ff36dc4dfb136eb702
Ly <34117007+LyingZz@users.noreply.github.com>
2022-01-25 17:17:37 +0800

    Create config.ts
    文档补充

--- config.ts
+++ config.ts
@@ -7,40 +7,40 @@ export default {
       {
         title: "Matrix World",
         cover: "/images/matrix.png",
-        link: '/',
+        link: 'https://www.matrixworld.org/home',
         statistics: [
           {
-            leading: "1",
-            sub: "Daap in the world"
+            leading: "Multichain",
+            sub: "first Turing-complete multichain metaverse"
           },
           {
-            leading: "10M+",
-            sub: "Daap in the world"
+            leading: "Partners",
+            sub: "Flow, Opensea, Rarible, Mask, Blocto, and more…"
           },
           {
-            leading: "$700M+",
-            sub: "Daap in the world"
+            leading: "$8M+",
+            sub: "in primary market sales"
           },
         ],
-        detail: "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain."
+        detail: "Matrix World is an exciting next-generation metaverse where users can create, connect, explore, and play like never before. The open world enables users to build immersive 3D applications on top of several different blockchains."
       },
       
       {
         title: "Phanta Bear",
         cover: "/images/bear.png",
-        link: '/',
+        link: 'https://www.ezek.io/',
         statistics: [
           {
-            leading: "1",
-            sub: "Daap in the world"
+            leading: "No. 1",
+            sub: "PFP NFT project in Asia"
           },
           {
-            leading: "10M+",
-            sub: "Daap in the world"
+            leading: "$9.6M+",
+            sub: "in primary market sales"
           },
           {
-            leading: "$700M+",
-            sub: "Daap in the world"
+            leading: "20K+ ETH",
+            sub: "trading volume in the first week"
           },
         ],
         detail: "The PhantaBear project was jointly launched by Jay Chou's PHANTACi and Ezek. PhantaBear is a collection of 10,000 algorithmically generated digital collectibles that double as membership cards for the Ezek Club. Each PhantaBear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner."
@@ -52,16 +52,16 @@ export default {
         link: '/',
         statistics: [
           {
-            leading: "1",
-            sub: "Daap in the world"
+            leading: "400+ ETH",
+            sub: "in primary market sales"
           },
           {
-            leading: "10M+",
-            sub: "Daap in the world"
+            leading: "1.2K+ ETH",
+            sub: "total trading volume"
           },
           {
-            leading: "$700M+",
-            sub: "Daap in the world"
+            leading: "No. 1",
+            sub: "spot reached on 1h Burn Leadboard of Ultra Sound Money"
           },
         ],
         detail: "Rivermen is an Oriental Cyber Theme Park based on blockchain technology, launched by the Cthuwork&NA team using elements from the voxel version of the “Along the River During Qingming Festival”. Each unique Rivermen NFT is a randomized 3D character model."
@@ -104,7 +104,6 @@ export default {
     ]
   },
   carrers: {
-    title: 'Carrers - Matrix Labs',
     leading: {
       desc: "Matrix Labs provides a range of benefits to support employee development and well-being. We value diversity and equality, employing people from different cultural and ethnical backgrounds. Other reasons for joing our team are:",
       reasons: [
@@ -119,7 +118,7 @@ export default {
       Art: [
         { 
           id: 0,
-          title: 'Test Art job',
+          title: 'Character Concept Artist',
           responsibility: [
             'Create characters based on the combined requirements of the Art Director and the game design team.',
             'Work with the Art Director and other team members to establish style guidelines and integrate them into the concept art.',
@@ -135,52 +134,80 @@ export default {
         },
         { 
           id: 1,
-          title: 'Character Concept Artist',
+          title: 'Environment Concept Artist',
           responsibility: [
-            'Create characters based on the combined requirements of the Art Director and the game design team.',
-            'Work with the Art Director and other team members to establish style guidelines and integrate them into the concept art.',
-            'Review concept art from art vendors and provide feedback.' 
+            'Work closely with the Art Director to decide the game style and formulate design guidelines and specifications for environment concept creation.',
+            'Coordinate between concept artists and the 3D environment team.',
+            'Create environment concept designs upon request.' ,
+            'Review the concept art submitted by outsourcing comapnies and provide feedback.' 
           ],
           requirments: [
-            'Solid painting skills and understanding of painting techniques.',
-            'Very familiar with Adobe Photoshop and other related software.',
-            'Proven record of meeting deadlines.',
-            'Familiar with different styles such as Western art styles and Cyberpunk.',
-            'Good communication, interpersonal skills and learning ability, and a high sense of responsibility.'
+            'Passionate about the gaming industry. Have a good understanding of what makes games visually appealing and fun to play.',
+            'Undergraduate or above degree, Art and Design majors preferred.',
+            'Solid drawing skills.',
+            'Past experience with environment concept design, team management experience preferred.',
+            'Analytical and innovative mentality preferred. Can accurately understand their role and fulfill it with high efficiency.',
+            'High sense of responsibility, good communication and interpersonal skills.',
+            'Team player.',
+            'Familiar with the software required for the position.',
+            'Comfortable with learning new tools and proactively tries to understand new technologies.'
+          ]
+        },
+        { 
+          id: 2,
+          title: '3D Modeler',
+          responsibility: [
+            'Create models and textures of buildings, vegetation, objects, etc..',
+            'Produce models that accurately portray the concept design.',
+            'Develop a standard workflow for model-creation with other team members.'
+          ],
+          requirments: [
+            'Experience with Maya, 3Dsmax, ZBrush, Substance Painter, etc..',
+            'Experience with Unity3D.',
+            'Good compressive skills, fast learner, motivated individual.',
+            'Must be comfortable with learning new tools and being able to apply them quickly to the job.',
+            'Experience in the gaming industry preferred.'
+          ]
+        },
+        { 
+          id: 3,
+          title: 'UI Designer',
+          responsibility: [
+            'Create intuitive and elegant in-game UI, focusing on player’s experience.',
+            'Design other promotional images, icons, etc..',
+            'Develop UI production process with other team members.'
+          ],
+          requirments: [
+            'Undergraduate or above degree in Art or Design.',
+            'Solid and proven experience in UI design.',
+            'Able to adapt to and create UI in different styles.',
+            'Familiar with Photoshop and Illustrator and other related software.',
+            'Interested in gaming and anime. Critical, creative, team player and good at communicating.',
+            'Experience in game UI projects preferred.'
           ]
         }
       ],
       Technology: [
         { 
           id: 0,
-          title: 'Test Technology job',
+          title: 'Senior Unity Developer',
           responsibility: [
-            'Create characters based on the combined requirements of the Art Director and the game design team.',
-            'Work with the Art Director and other team members to establish style guidelines and integrate them into the concept art.',
-            'Review concept art from art vendors and provide feedback.' 
+            'Coordinate the work of other developers and provide mentorship and support for less experienced teammates.',
+            'Hands on implementation of features and updates with other developers.',
+            'Create technical designs for reusable white label systems that meet current requirements and anticipate future needs.' ,
+            'Work with back-end developers for seamless integration of features and live ops.' ,
+            'Build a deep understanding of core systems and architecture to provide quality benchmarking on process, updates and optimizations.' ,
+            'Uphold code quality and code reviews while ensuring features are delivered on time.' 
           ],
           requirments: [
-            'Solid painting skills and understanding of painting techniques.',
-            'Very familiar with Adobe Photoshop and other related software.',
-            'Proven record of meeting deadlines.',
-            'Familiar with different styles such as Western art styles and Cyberpunk.',
-            'Good communication, interpersonal skills and learning ability, and a high sense of responsibility.'
-          ]
-        },
-        { 
-          id: 1,
-          title: 'Character Concept Artist',
-          responsibility: [
-            'Create characters based on the combined requirements of the Art Director and the game design team.',
-            'Work with the Art Director and other team members to establish style guidelines and integrate them into the concept art.',
-            'Review concept art from art vendors and provide feedback.' 
-          ],
-          requirments: [
-            'Solid painting skills and understanding of painting techniques.',
-            'Very familiar with Adobe Photoshop and other related software.',
-            'Proven record of meeting deadlines.',
-            'Familiar with different styles such as Western art styles and Cyberpunk.',
-            'Good communication, interpersonal skills and learning ability, and a high sense of responsibility.'
+            'Bachelor’s Degree or better in Computer Science.',
+            'Minimum 5 years of commercial game programming experience with 2+ in a leadership or co-leadership level role.',
+            'Minimum 3 years of game development experience with Unity.',
+            'Strong programming skills in Unity and C#.',
+            'Broad knowledge of game architecture and technologies.',
+            'Experience with debugging, profiling and performance optimizations and ability to coach other devs in these skills.',
+            'Experienced in Unity Network frameworks (i.e. Fonton, Mirror, Normcore).',
+            'Successfully launched at least 1 Unity games as a tech lead.',
           ]
         }
       ]
