3075adcf55056a5ad301f56102e14d63050a4bcc
xia <xiadd0102@gmail.com>
2022-01-26 09:44:21 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export default {
   home: {
     title: "Home - Matrix Labs",
-    "leading-title": "Bringing the mataverse to life",
+    "leading-title": "Bringing the metaverse to life",
     "leading-text": "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain.",
     projects: [
       {
