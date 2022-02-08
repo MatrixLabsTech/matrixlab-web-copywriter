6151a1b1586a0b41e9a9b7975ae85203519d6bd6
Ly <34117007+LyingZz@users.noreply.github.com>
2022-02-08 15:41:50 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export default {
   home: {
     title: "Home - Matrix Labs",
-    email: "lambo6364@gmail.com",
+    email: "wusheng@whitematrix.io",
     "leading-title": "Bringing the metaverse to life",
     "leading-text": "The Matrix Labs team is a group of experienced blockchain developers. They are partnering with Ezek and PHANTACi to bring the Phanta Bear NFT Collection to life on the blockchain.",
     projects: [
